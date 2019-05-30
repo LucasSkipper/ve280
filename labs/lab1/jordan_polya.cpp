6e7f0799886af6518ad8e43112fbd04dfb208fee
LucasSkipper <1158757779@qq.com>
2019-05-30 19:30:08 +0800

    Update jordan_polya.cpp

--- labs/lab1/jordan_polya.cpp
+++ labs/lab1/jordan_polya.cpp
@@ -4,9 +4,18 @@
 using namespace std;
 
 bool is_jordan_polya(int num) {
-    // TODO: Your implementation here
-    return false;
+    int a[30],na=0,fac=1;
+    while (sum<num){
+        na++;
+        fac=fac*na;
+        a[na]=fac;
+    }
+    for (int i=na;i>=2;i--){
+        while (num%a[i]==0) num=num/a[i];
+    }
+    return (num==1);
 }
+
 
 void test_jordan_polya() {
     assert(!is_jordan_polya(5));  // 5 is not a Jordan-Polya number
